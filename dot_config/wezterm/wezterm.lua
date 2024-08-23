f44edcf8aa31ce959e51d5cfa30594233c246761
Gavin Hailey <gavin.hailey@ibotta.com>
2024-08-23 13:21:49 -0600

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,17 +1,17 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local selected_scheme = 'Tokyo Night'
+local selected_scheme = "Tokyo Night"
 local scheme = wezterm.get_builtin_color_schemes()[selected_scheme]
 
 scheme.tab_bar.inactive_tab_hover.italic = true
 scheme.tab_bar.active_tab.bg_color = scheme.selection_bg
 
-wezterm.log_info(scheme)
-
 local config = wezterm.config_builder()
 config:set_strict_mode(true)
 
-config.font = wezterm.font 'MesloLGS Nerd Font Mono'
+config.audible_bell = "Disabled"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 12.0
 config.color_scheme = selected_scheme
 
@@ -21,16 +21,16 @@ config.text_background_opacity = 0.75
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
 config.tab_max_width = 23
 
 config.window_frame = {
-  font = wezterm.font { family = 'MesloLGS Nerd Font Mono', weight = 'Bold' },
+	font = wezterm.font({ family = "MesloLGS Nerd Font Mono", weight = "Bold" }),
 }
 
 config.color_schemes = {
-  [selected_scheme] = scheme,
+	[selected_scheme] = scheme,
 }
 
 return config
